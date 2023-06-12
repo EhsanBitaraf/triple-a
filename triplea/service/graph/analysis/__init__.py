0fdc0aba8ae964b8b0a3b507138a4a6ae6841a83
EhsanBitaraf <bitaraf.e@iums.ac.ir>
2023-06-12 15:43:21 +0330

    Fix info() path

--- triplea/service/graph/analysis/__init__.py
+++ triplea/service/graph/analysis/__init__.py
@@ -1,11 +1,12 @@
 from triplea.service.graph.analysis.ganalysis import (
-    info,
     sorted_degree_centrality,
     show_degree_distribution,
     sorted_average_neighbor_degree,
     get_avg_shortest_path_length_per_node,
 )
 
+
+from triplea.service.graph.analysis.info import info
 from triplea.service.graph.analysis.diameter import graph_diameter
 
 from triplea.service.graph.analysis.neighbor_number import average_neighbor_number
